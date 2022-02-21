d941450ce17298d6ea67303f3b193a5d537308cb
Mauricio Pereira <61766272+mauricioprb@users.noreply.github.com>
2022-02-21 10:33:51 -0300

    ex02.c

---
+++ ex02.c
@@ -0,0 +1,9 @@
+// Construir um programa que receba uma altura e peso de um possivel atleta
+
+// função  recebe altura e peso e devolve esporte
+
+//  se altura < 1.80 e peso < 70
+// retornar atletismo
+
+// se altura > 1.80 e peso < 70
+// retornar volei ou basquete se não reornar futebol
