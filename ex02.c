23197cb8ed2f54c407670d206f0f112fdb5f448d
Mauricio Pereira <61766272+mauricioprb@users.noreply.github.com>
2022-02-21 10:48:36 -0300

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -6,4 +6,32 @@
 // retornar atletismo
 
 // se altura > 1.80 e peso < 70
-// retornar volei ou basquete se não reornar futebol
+// retornar volei ou basquete se não retornar futebol
+
+#include <stdio.h>
+
+int esporte(){
+  char atletismo, volei, basquete, futebol;
+  float altura, peso;
+
+  printf("Altura: ");
+  scanf("%f", &altura);
+  printf("Peso: ");
+  scanf("%f", &peso);
+
+  if(altura < 1.80 && peso < 70){
+    return atletismo;
+  }
+  if(altura < 1.80 && peso < 70){
+    return volei;
+    return basquete;
+  }
+  else{
+    return futebol;
+  }
+}
+int main(){
+  esporte();
+  
+  return 0;
+}
