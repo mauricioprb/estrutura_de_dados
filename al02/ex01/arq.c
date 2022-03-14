820a6823a4d87a20ea4c5a5d19eff02e673195df
mauricio <mauprb@gmail.com>
2022-03-14 10:44:44 -0300

    parted

---
+++ al02/ex01/arq.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *procurador;
+    char nomeArquivo[200], linha[200];
+
+    printf("Nome arquivo: ");
+    scanf("%s", nomeArquivo);
+
+    procurador = fopen(nomeArquivo, "r");
+
+    if(!procurador){
+        printf("Arquivo não encontrado\n");
+        exit(0);
+    }
+    do{
+        fscanf(procurador, "%s", linha);
+        printf("%s\n", linha);
+    }while(!feof(procurador));
+
+    fclose(procurador);
+
+    return 1;
+}
