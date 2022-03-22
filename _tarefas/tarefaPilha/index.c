acc0209c26c679d24c287c1402b1f70ff829a865
mauricio <mauriciopereirabraga@gmail.com>
2022-03-21 22:47:49 -0300

    tarefaPIlha

---
+++ _tarefas/tarefaPilha/index.c
@@ -0,0 +1,60 @@
+/*
+Fazer um programa em C ou C++ que leia um arquivo com N n° inteiros (um abaixo do outro)
+e os popule em uma uma pilha encadeada dinamicamente. Ao final, exiba
+quantos elementos há na pilha e seu topo (último valor).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+    int dado;
+    struct no *prox;
+} Celula;
+
+Celula *inserir(int valor, Celula *topo){
+    Celula *novo = (Celula *)malloc(sizeof(Celula));
+    novo->dado = valor;
+    novo->prox = topo;
+    return novo;
+}
+
+int contarNos(Celula *topo){
+    int qtd = 0;
+    Celula *p;
+    for (p = topo; p; p = p->prox){
+        qtd++;
+    }
+    return qtd;
+}
+
+int main(){
+	Celula *pilha = NULL; 
+    Celula *pilhaImpar = NULL;
+	FILE *procurador;
+	char nomeArquivo[200];
+    int valor;
+
+	printf("Nome do arquivo: ");
+	scanf("%s", nomeArquivo);
+
+	procurador = fopen(nomeArquivo, "r");
+
+	if (!procurador){
+		printf("Arquivo não encontrado\n");
+		exit(0);
+	}
+
+	do{
+		fscanf(procurador, "%d", &valor);
+		pilha = inserir(valor, pilha);
+	} while(!feof(procurador));
+
+	fclose(procurador);
+
+	printf("\nEndereço do último elemento: %p\n", pilha);
+    printf("Valor do último elemento: %d\n", pilha->dado);
+    printf("O tamanho da pilha um é: %d\n", contarNos(pilha));
+
+	return 1;
+}
