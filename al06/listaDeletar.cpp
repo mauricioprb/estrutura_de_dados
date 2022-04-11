446375347c8c4d04b3af20719028329d7540292e
mauricioprb <mauriciopereirabraga@gmail.com>
2022-04-11 10:36:19 -0300

    al06

---
+++ al06/listaDeletar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "source.h"
+
+using namespace std;
+
+int main(){
+    Celula *lista = NULL;
+    int qtdNumerosLista = 10; 
+
+    lista = popular(lista, qtdNumerosLista);
+    cout << "Lista inicial:" << endl;
+    exibirLista(lista);
+
+    int numero;
+    cout << "Qual número eseja apagar da lista? ";
+    cin >> numero;
+
+    lista = excluirOcorrencia(numero, lista);
+    cout << "\nLista após a exclusão:" << endl;
+    exibirLista(lista);
+
+    return 1;
+}
